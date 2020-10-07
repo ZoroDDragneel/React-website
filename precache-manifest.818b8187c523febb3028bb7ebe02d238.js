4cb063d51f820514e706e8b8258c2513f0591409
ZoroDDragneel <kopane12345@gmail.com>
2020-10-08 01:24:18 +0200

    Updates

---
+++ precache-manifest.818b8187c523febb3028bb7ebe02d238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d369c1bb601d264efc4df2032892c148",
+    "url": "/React-website/index.html"
+  },
+  {
+    "revision": "7d723295c92160aa9623",
+    "url": "/React-website/static/css/main.eaabdccb.chunk.css"
+  },
+  {
+    "revision": "c2d7225a37f35af651bb",
+    "url": "/React-website/static/js/2.9af61612.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-website/static/js/2.9af61612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d723295c92160aa9623",
+    "url": "/React-website/static/js/main.2a81304b.chunk.js"
+  },
+  {
+    "revision": "bf6a119892f5e3357912",
+    "url": "/React-website/static/js/runtime-main.4d3a8dba.js"
+  }
+]);
